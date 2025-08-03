1b0667d86c4b5d104a694ad535b75cb5e83e8939
Adam Parr <adamparr.uk@gmail.com>
2025-08-03 12:44:28 +0100

    Deserialise integer, refactor

--- src/tests/test_deserialises_simple_string.py
+++ src/tests/test_deserialises_simple_string.py
@@ -43,38 +43,8 @@ def test_deserialises_simple_string_with_only_spaces() -> None:
 
     assert deserialised_message == expected_message
 
-def test_raises_exception_for_empty_message() -> None:
-    sent_message = ""
-
-    with pytest.raises(InvalidMessageError):
-        deserialise(sent_message)
-
 def test_raises_exception_for_no_data() -> None:
     sent_message = "+\r\n"
-
-    with pytest.raises(InvalidMessageError):
-        deserialise(sent_message)
-
-def test_raises_exception_for_data_after_crlf() -> None:
-    sent_message = "+hello world\r\nabc"
-
-    with pytest.raises(InvalidMessageError):
-        deserialise(sent_message)
-
-def test_raises_exception_for_missing_crlf() -> None:
-    sent_message = "+hello world"
-
-    with pytest.raises(InvalidMessageError):
-        deserialise(sent_message)
-
-def test_raises_exception_for_missing_prefix() -> None:
-    sent_message = "\r\nhello world\r\n"
-
-    with pytest.raises(InvalidMessageError):
-        deserialise(sent_message)
-
-def test_raises_exception_for_invalid_prefix() -> None:
-    sent_message = "*hello world\r\n"
 
     with pytest.raises(InvalidMessageError):
         deserialise(sent_message)
--- src/whodis/deserialise.py
+++ src/whodis/deserialise.py
@@ -10,20 +10,29 @@ def parse_simple_string(line: str) -> str:
         raise InvalidMessageError
     return line[1:]
 
-def parse(lines: list[str]) -> str:
-    if not lines:
+def parse_integer(line: str) -> int:
+    # Cannot accept only one char (the prefix)
+    if len(line) == 1:
+        raise InvalidMessageError
+    try:
+        return int(line[1:])
+    except ValueError as e:
+        raise InvalidMessageError from e
+
+def parse(lines: list[str]) -> str | int:
+    # The following parsers expect only one line
+    if len(lines) != 1:
         raise InvalidMessageError
 
     line = lines[0]
     if line.startswith("+"):
-        # Cannot accept more than one line for a simple string
-        if len(lines) > 1:
-            raise InvalidMessageError
         return parse_simple_string(line)
+    if line.startswith(":"):
+        return parse_integer(line)
 
     raise InvalidMessageError
 
-def deserialise(msg: str) -> str:
+def deserialise(msg: str) -> str | int:
     if not msg.endswith(CRLF):
         raise InvalidMessageError
 

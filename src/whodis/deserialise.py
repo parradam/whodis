22a8dd751c90cf48faef0f70de670b5f0c5873c0
Adam Parr <adamparr.uk@gmail.com>
2025-08-04 21:16:35 +0100

    Deserialise bulk string, refactor

--- src/whodis/deserialise.py
+++ src/whodis/deserialise.py
@@ -4,37 +4,82 @@ CRLF = "\r\n"
 class InvalidMessageError(ValueError):
     pass
 
-def parse_simple_string(line: str) -> str:
-    # Cannot accept only one char (the prefix)
-    if len(line) == 1:
-        raise InvalidMessageError
-    return line[1:]
+def parse_bulk_string(msg: str) -> str:
+    if not msg.startswith("$") or not msg.endswith(CRLF):
+        error = "Message could not be parsed as a bulk string"
+        raise InvalidMessageError(error)
 
-def parse_integer(line: str) -> int:
-    # Cannot accept only one char (the prefix)
-    if len(line) == 1:
-        raise InvalidMessageError
     try:
-        return int(line[1:])
+        prefix_end = msg.index(CRLF)
+        num_chars = int(msg[1:prefix_end])
+    except (ValueError, IndexError) as e:
+        error = "Content length could not be determined for bulk string"
+        raise InvalidMessageError(error) from e
+
+    # Don't allow null bulk strings; this isn't RESP3, we're better than that
+    if num_chars < 0:
+        error = "Content length is negative"
+        raise InvalidMessageError(error)
+
+    content_start = prefix_end + len(CRLF)
+    content_end = content_start + num_chars
+
+    if len(msg) != content_end + len(CRLF):
+        error = "Content length does not match number of bytes"
+        raise InvalidMessageError(error)
+
+    if CRLF in msg[content_start:content_end]:
+        error = "Content contains newline characters"
+        raise InvalidMessageError(error)
+
+    return msg[content_start:content_end]
+
+def parse_simple_string(msg: str) -> str:
+    if not msg.startswith("+") or not msg.endswith(CRLF):
+        error = "Message could not be parsed as a simple string"
+        raise InvalidMessageError(error)
+
+    extracted = msg.removeprefix("+").removesuffix(CRLF)
+    if len(extracted) == 0:
+        error = "Content length is zero"
+        raise InvalidMessageError(error)
+
+    if CRLF in extracted:
+        error = "Content contains newline characters"
+        raise InvalidMessageError(error)
+
+    return extracted
+
+def parse_integer(msg: str) -> int:
+    if not msg.startswith(":") or not msg.endswith(CRLF):
+        error = "Message coudld not be parsed as an integer"
+        raise InvalidMessageError(error)
+
+    extracted = msg.removeprefix(":").removesuffix(CRLF)
+    if len(extracted) == 0:
+        error = "Content length is zero"
+        raise InvalidMessageError(error)
+
+    try:
+        return int(extracted)
     except ValueError as e:
-        raise InvalidMessageError from e
+        error = "Could not parse integer"
+        raise InvalidMessageError(error) from e
 
-def parse(lines: list[str]) -> str | int:
-    # The following parsers expect only one line
-    if len(lines) != 1:
-        raise InvalidMessageError
-
-    line = lines[0]
-    if line.startswith("+"):
-        return parse_simple_string(line)
-    if line.startswith(":"):
-        return parse_integer(line)
-
-    raise InvalidMessageError
+def parse(msg: str) -> str | int:
+    match msg[0]:
+        case "$":
+            return parse_bulk_string(msg)
+        case "+":
+            return parse_simple_string(msg)
+        case ":":
+            return parse_integer(msg)
+        case _:
+            error = "Message could not be parsed: unsupported data type"
+            raise InvalidMessageError(error)
 
 def deserialise(msg: str) -> str | int:
     if not msg.endswith(CRLF):
-        raise InvalidMessageError
-
-    lines = msg.rstrip(CRLF).split(CRLF)
-    return parse(lines)
+        error = "Message cannot be parsed: missing CRLF terminator"
+        raise InvalidMessageError(error)
+    return parse(msg)

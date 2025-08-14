fe59de2c92b76f42731a31a0718ff225ea7733b3
Adam Parr <adamparr.uk@gmail.com>
2025-08-14 21:18:23 +0100

    Serialise integers

--- src/tests/test_serialise.py
+++ src/tests/test_serialise.py
@@ -1,7 +1,17 @@
 import pytest
 
-from src.whodis.serialise import SerialiseResult, serialise
+from src.whodis.serialise import SerialiseError, SerialiseResult, serialise
 
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        pytest.param({"a": 1}, id="protocol_unsupported_data_type"),
+    ],
+)
+def test_invalid_protocol_messages(data: str) -> None:
+    with pytest.raises(SerialiseError):
+        serialise(data)
 
 @pytest.mark.parametrize(
     ("data", "expected_serialise_result"),
@@ -14,5 +24,16 @@ from src.whodis.serialise import SerialiseResult, serialise
     ],
 )
 def test_valid_simple_strings(data: str, expected_serialise_result: str) -> None:
+    serialise_result = serialise(data)
+    assert serialise_result == expected_serialise_result
+
+@pytest.mark.parametrize(
+    ("data", "expected_serialise_result"),
+    [
+        pytest.param(123, SerialiseResult(":123\r\n", 6), id="integer_content"),
+        pytest.param(2**63, SerialiseResult(":9223372036854775808\r\n", 22), id="integer_large"),
+    ],
+)
+def test_valid_integers(data: str, expected_serialise_result: str) -> None:
     serialise_result = serialise(data)
     assert serialise_result == expected_serialise_result
--- src/whodis/serialise.py
+++ src/whodis/serialise.py
@@ -8,11 +8,23 @@ class SerialiseResult:
     message: str
     bytes_produced: int
 
+class SerialiseError(ValueError):
+    pass
+
 
 def serialise(data: RESPDataType) -> SerialiseResult:
-    return _serialise_simple_string(data)
+    if isinstance(data, str):
+        return _serialise_simple_string(data)
+    if isinstance(data, int):
+        return _serialise_integer(data)
+    error = "Message could not be serialised: unsupported data type"
+    raise SerialiseError(error)
 
 
 def _serialise_simple_string(data: RESPDataType) -> SerialiseResult:
     message = f"+{data}{CRLF}"
+    return SerialiseResult(message, len(message))
+
+def _serialise_integer(data: RESPDataType) -> SerialiseResult:
+    message = f":{data!s}{CRLF}"
     return SerialiseResult(message, len(message))

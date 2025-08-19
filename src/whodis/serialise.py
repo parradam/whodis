1bf7d17c300cd1c04ed74de6522e97b915dba971
Adam Parr <adamparr.uk@gmail.com>
2025-08-19 16:31:55 +0100

    Serialise arrays

--- src/whodis/serialise.py
+++ src/whodis/serialise.py
@@ -8,34 +8,47 @@ class Kind(Enum):
     PROTOCOL = auto()
     NON_PROTOCOL = auto()
 
+
 @dataclass(frozen=True)
 class SerialiseResult:
     message: str
     bytes_produced: int
+
 
 class SerialiseError(ValueError):
     pass
 
 
 def serialise(data: RESPDataType, kind: Kind = Kind.NON_PROTOCOL) -> SerialiseResult:
-    if isinstance(data, str) and kind == Kind.PROTOCOL:
-        return _serialise_simple_string(data)
     if isinstance(data, str):
+        if kind == Kind.PROTOCOL:
+            return _serialise_simple_string(data)
         return _serialise_bulk_string(data)
-    if isinstance(data, int):
+    if isinstance(data, list):
+        return _serialise_array(data, kind)
+    if isinstance(data, int):  # pyright: ignore[reportUnnecessaryIsInstance]
         return _serialise_integer(data)
-    error = "Message could not be serialised: unsupported data type"
+    error = f"Message could not be serialised: unsupported data {type(data)}"
     raise SerialiseError(error)
 
 
-def _serialise_simple_string(data: RESPDataType) -> SerialiseResult:
+def _serialise_simple_string(data: str) -> SerialiseResult:
     message = f"+{data}{CRLF}"
     return SerialiseResult(message, len(message))
+
 
 def _serialise_bulk_string(data: str) -> SerialiseResult:
     message = f"${len(data)}{CRLF}{data}{CRLF}"
     return SerialiseResult(message, len(message))
 
-def _serialise_integer(data: RESPDataType) -> SerialiseResult:
+
+def _serialise_array(data: list[RESPDataType], kind: Kind) -> SerialiseResult:
+    parts = [f"*{len(data)}{CRLF}"]
+    parts.extend(serialise(d, kind).message for d in data)
+    message = "".join(parts)
+    return SerialiseResult(message, len(message))
+
+
+def _serialise_integer(data: int) -> SerialiseResult:
     message = f":{data!s}{CRLF}"
     return SerialiseResult(message, len(message))
